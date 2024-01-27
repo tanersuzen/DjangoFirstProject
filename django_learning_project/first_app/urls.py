e1a025f960929358956dd0142eac902d067b15e3
Taner Süzen <taner.suzen95@gmail.com>
2024-01-27 12:56:42 +0300

    Commit before Template

--- django_learning_project/first_app/urls.py
+++ django_learning_project/first_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("about", views.index, name="index"),
-]
+    path("", views.index, name="index"), #tanersuzen.com/first_app
+    path("<int:number1>/", views.course_number_view, name="coursenumberview"), #tanersuzen.com/first_app/10 -> tanersuzen.com/first_app/kotlin
+    path("<str:item>/", views.course, name="course"), #tanersuzen.com/first_app/python
+    path("<int:num1>/<int:num2>/", views.multiply_view, name="multiply") #tanersuzen.com/first_app/5/4
+
+    ]
--- django_learning_project/first_app/views.py
+++ django_learning_project/first_app/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+
+course_dic = {
+    "python" : "Python Course Page",
+    "java" : "Java Course Page",
+    "kotlin" : "Kotlin Course Page",
+    "swift" : "Swift Course Page"
+}
 
 def index(request):
     return HttpResponse("This is first Django project, first index")
+
+def course(request, item):
+    try:
+        course1 = course_dic[item]
+        return HttpResponse(course1)
+    except:
+        return HttpResponseNotFound("Not Found! Please look for another course!")
+        #raise Http404("Not Found! Please look for another course!")
+    #return HttpResponse(course_dic.get(item,"Not Found!"))
+
+def multiply_view(request, num1, num2):
+    result = num1 * num2
+    return HttpResponse(f"{num1} * {num2} = {num1*num2}")
+
+def course_number_view(request,number1):
+    course_list = list(course_dic.keys())
+    try:
+        course_name = course_list[number1]
+
+        page_to_go = reverse("course",args=[course_name])
+        return HttpResponseRedirect(page_to_go)
+    except:
+        return HttpResponseNotFound("Not Found! Please look for another course!")
